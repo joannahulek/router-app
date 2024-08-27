e41ee25dd3086376d76791c508b90272aa01177d
Joanna Hulek <joanna.hulek@gmail.com>
2024-08-27 09:46:34 +0200

    not-found component added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {RouterModule, Routes} from "@angular/router";
-import {FirstComponent} from "../first/first.component";
-import {SecondComponent} from "../second/second.component";
+import {FirstComponent} from "./first/first.component";
+import {SecondComponent} from "./second/second.component";
+import {NotFoundComponent} from "./not-found/not-found.component";
 
 const routes: Routes = [
   {path: 'first', component: FirstComponent},
-  {path: 'second', component: SecondComponent}
+  {path: 'second', component: SecondComponent},
+  {path: '**', component: NotFoundComponent}
 ]
 
 @NgModule({
